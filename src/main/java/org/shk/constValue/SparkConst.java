103e949e044656b3c9b5f6cd0f86e0bc03b7cd48
shenhangke <475162739@qq.com>
2019-04-23 12:55:13 +0800

    delete local model

--- src/main/java/org/shk/constValue/SparkConst.java
+++ src/main/java/org/shk/constValue/SparkConst.java
@@ -3,5 +3,5 @@ package org.shk.constValue;
 import org.apache.spark.sql.SparkSession;
 
 public class SparkConst {
-	public static final SparkSession MainSession=SparkSession.builder().master("local").appName("shk_WikiAnalyse").getOrCreate();
+	public static final SparkSession MainSession=SparkSession.builder().appName("shk_WikiAnalyse").getOrCreate();
 }
