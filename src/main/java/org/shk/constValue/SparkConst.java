08866bd960eb116ca5540f254037b61a5b2db793
shenhangke <475162739@qq.com>
2019-05-06 10:49:32 +0800

    recommit to check the error

--- src/main/java/org/shk/constValue/SparkConst.java
+++ src/main/java/org/shk/constValue/SparkConst.java
@@ -8,8 +8,12 @@ public class SparkConst {
 		File,Database;
 	} 
 	
-	public static final SparkSession MainSession=SparkSession.builder().appName("shk_WikiAnalyse").
-			config("spark.executor.extraJavaOptions","-verbose:gc -XX:+PrintGCDetails -XX:+PrintGCTimeStamps").getOrCreate();
+	public static final boolean RunOnLocal=true;
+	
+	public static final SparkSession MainSession=SparkSession.builder().appName("shk_WikiAnalyse").getOrCreate();
+	
+	/*public static final SparkSession LocalSession=SparkSession.builder().appName("shk_WikiAnalyse").master("local[3]").config("spark.executor.memory","3g").
+			config("spark.driver.memory","4g").getOrCreate();*/
 	
 	static{
 		JavaSparkContext javaSparkcontext=new JavaSparkContext(MainSession.sparkContext());
