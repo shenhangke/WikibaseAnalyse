2c549ba0b0115224b301e3014840392779abef60
shenhangke <475162739@qq.com>
2019-05-06 10:26:20 +0800

    modify the executor memory and recommit

--- src/main/java/org/shk/constValue/SparkConst.java
+++ src/main/java/org/shk/constValue/SparkConst.java
@@ -1,5 +1,6 @@
 package org.shk.constValue;
 
+import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.SparkSession;
 
 public class SparkConst {
@@ -7,7 +8,12 @@ public class SparkConst {
 		File,Database;
 	} 
 	
-	public static final SparkSession MainSession=SparkSession.builder().master("yarn").appName("shk_WikiAnalyse").config("spark.executor.memory","8g").
+	public static final SparkSession MainSession=SparkSession.builder().master("yarn").appName("shk_WikiAnalyse").config("spark.executor.memory","7g").
 			config("spark.executor.extraJavaOptions","-verbose:gc -XX:+PrintGCDetails -XX:+PrintGCTimeStamps").getOrCreate();
+	
+	static{
+		JavaSparkContext javaSparkcontext=new JavaSparkContext(MainSession.sparkContext());
+		javaSparkcontext.setLogLevel("WARN");
+	}
 	//public static final SparkSession TestSession=SparkSession.builder().master("local[3]").appName("shk_WikiAnalyse").getOrCreate();
 }
