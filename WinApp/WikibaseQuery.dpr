53c2381a245f3e79f700da521a201a69d07077ce
shenhangke <475162739@qq.com>
2019-05-22 21:17:23 +0800

    modify the local model to server

---
+++ WinApp/WikibaseQuery.dpr
@@ -0,0 +1,14 @@
+program WikibaseQuery;
+
+uses
+  Vcl.Forms,
+  MainWin in 'MainWin.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
